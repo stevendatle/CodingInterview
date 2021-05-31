7428e6f05b34efb35fbdde4e5ed9c7ae17bc0467
stevendatle <stevendatle@yahoo.com>
2021-05-31 19:20:16 -0400

    reworked fetch api data - created new function to correctly render data

--- js/scripts.js
+++ js/scripts.js
@@ -14,33 +14,68 @@ function employeeData() {
       return response.json();
     })
     //doing stuff with the data
-    .then((data) => {
-      //transforming json data into array
-      const objectArray = Object.entries(data);
+    .then(function (data) {
+      for (const key in data) {
+        if (Object.hasOwnProperty.call(data, key)) {
+          //we have the data!
+          let employee = data[key];
+          console.log(employee);
 
-      const html = objectArray
-        .map((employee) => {
-          //getting id and then inserting id into image url
-          let id = employee[1].employeeid;
-          let image = `http://sandbox.bittsdevelopment.com/code1/employeepics/${id}.jpg`;
+          //assigning api data to variables to be able to use them in our functions
+          let id = employee["employeeid"];
+          let fname = employee["employeefname"];
+          let lname = employee["employeelname"];
+          let bio = employee["employeebio"];
+          let roles = employee["roles"];
+          let featured = employee["employeeisfeatured"];
 
-          console.log(image);
-          return `
-        <div class="teamBox">
-          <p><img src="${image}" alt="${employee[1].employeefname}" /></p>
-          <h2>${employee[1].employeefname}</h2>
-          <p id="memberBio">${employee[1].employeebio}</p>
-        
-        `;
-        })
-        .join("");
-      console.log(html);
-      document
-        .querySelector("#container")
-        .insertAdjacentHTML("afterbegin", html);
+          console.log(id);
+
+          renderData(id, fname, lname, bio, roles, featured);
+        }
+      }
     })
     .catch(function (error) {
       //catching errors
       console.log("error!", error);
     });
+}
+
+function renderData(
+  id,
+  fname,
+  lname,
+  employeeBio,
+  employeeRoles,
+  employeeFeatured
+) {
+  //creating all the html elements that will be used
+  //box will be the employee card
+  let box = document.createElement("div");
+  let image = document.createElement("img");
+  let name = document.createElement("h2");
+  let bio = document.createElement("p");
+  let roles = document.createElement("div");
+  let crown = document.createElement("span");
+
+  //adding the elements to https://www.w3schools.com/jsref/prop_element_classlist.asp
+  box.classList.add("box");
+
+  //getting the image
+  image.src = `http://sandbox.bittsdevelopment.com/code1/employeepics/${id}.jpg`;
+  image.alt = `${fname} ${lname}`;
+
+  //putting api data into name, bio and pic
+  name.innerHTML = `${fname} ${lname}`;
+  bio.innerHTML = `${employeeBio}`;
+  console.log(name);
+
+  //adding items to container element
+  box.appendChild(image);
+  box.appendChild(name);
+  box.appendChild(bio);
+
+  //getting the container html element and adding the box to it
+  var getContainer = document.getElementById("container");
+  getContainer.appendChild(box);
 }
